a2627c4be3fe908462517ada456ab5ab6ed6a96e
ward <ward.van.bever@gmail.com>
2025-03-01 13:07:58 +0100

    save main.ts state with invalid store implementation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
+import { createSSRApp } from 'vue'
 import App from './App.vue'
+import { createRouter } from './router'
+import { createStore } from './store'
 
-createApp(App).mount('#app')
+// Export a factory function for creating new app, router and store instances
+export function createApp() {
+  const app = createSSRApp(App)
+  const router = createRouter()
+  const store = createStore()
+
+  app.use(router)
+  app.use(store)
+
+  return { app, router, store }
+}
+
+
+// TODO which formatter is actually being used by vscode on save? why is it not adding ";"? 
