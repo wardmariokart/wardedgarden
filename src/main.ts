8968b4b569dcf6635587a1f969b0bd2ccae72f12
ward <ward.van.bever@gmail.com>
2025-03-01 13:09:48 +0100

    main.ts: remove invalid store impl

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 import { createRouter } from './router'
-import { createStore } from './store'
 
 // Export a factory function for creating new app, router and store instances
 export function createApp() {
   const app = createSSRApp(App)
   const router = createRouter()
-  const store = createStore()
 
   app.use(router)
-  app.use(store)
 
-  return { app, router, store }
+  return { app, router }
 }
 
 
